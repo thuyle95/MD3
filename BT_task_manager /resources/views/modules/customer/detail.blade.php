d57905e84c2447c15f4223e59ba0e4e78292af00
thuyle95 <thuyle29111995@gmail.com>
2022-01-26 14:00:29 +0700

    bt

---
+++ BT_task_manager /resources/views/modules/customer/detail.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chi tiết khách hàng</title>
+</head>
+
+<body>
+    <h1>Chi tiết khách hàng</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>STT</th>
+                <th>Họ và tên</th>
+                <th>Số điện thoại</th>
+                <th>Email</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Nguyễn Văn A</td>
+                <td>01234567890</td>
+                <td>email.test@mail.com</td>
+                <td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
---
+++ BT_task_manager /resources/views/modules/customer/edit.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chỉnh sửa thông tin</title>
+</head>
+<body>
+    <h1>Thay đổi thông tin</h1>
+    <table border="1">
+        <thead>
+            <tr>
+                <th>STT</th>
+                <th>Họ và tên</th>
+                <th>Số điện thoại</th>
+                <th>Email</th>
+                <th>Thao tác</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td><input type="text" placeholder="Nguyễn Văn A"></td>
+                <td><input type="text" placeholder="01234567890"></td>
+                <td><input type="text" placeholder="email.test@mail.com"></td>
+                <td><button type="submit">Lưu</button></td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
