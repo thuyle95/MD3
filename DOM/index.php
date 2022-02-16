c45541450d0d25210396a744a1ac87b0dfe343a0
thuyle95 <thuyle29111995@gmail.com>
2022-02-16 17:07:50 +0700

    jquery

---
+++ DOM/index.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+    <div id="myDIV" class="header">
+        <h3 style="margin:5px">My To Do List</h3>
+        <input type="text" id="myInput" placeholder="Title...">
+        <span onclick="newElement()" class="addBtn btn btn-danger">Add</span>
+    </div>
+    <div class="content">
+        <ul class="list-group" id="myUL">
+            <li class="list-group-item">Hit the gym</li>
+            <li class="list-group-item">Pay bills</li>
+            <li class="list-group-item">Meet George</li>
+            <li class="list-group-item">Buy eggs</li>
+            <li class="list-group-item">Read a book</li>
+            <li class="list-group-item">Organize</li>
+
+        </ul>
+    </div>
+    <script>
+        var myNodelist = document.getElementsByTagName("LI");
+        for (i = 0; i < myNodelist.length; i++) {
+            var span = document.createElement("SPAN");
+            var txt = document.createTextNode("\u00D7");
+            span.className = "close";
+            span.appendChild(txt);
+            myNodelist[i].appendChild(span);
+        }
+
+        var close = document.getElementsByClassName("close");
+        closeElement();
+
+        function closeElement() {
+            var i;
+            for (i = 0; i < close.length; i++) {
+                close[i].onclick = function() {
+                    var div = this.parentElement;
+                    div.style.display = "none";
+                }
+            }
+        }
+
+        function newElement() {
+            var li = document.createElement("li");
+            var inputValue = document.getElementById("myInput").value;
+            var t = document.createTextNode(inputValue);
+            li.appendChild(t);
+            if (inputValue === '') {
+                alert("You must write something!");
+            } else {
+                document.getElementById("myUL").appendChild(li);
+            }
+            document.getElementById("myInput").value = "";
+            var span = document.createElement("SPAN");
+            var txt = document.createTextNode("\u00D7");
+            span.className = "close";
+            span.appendChild(txt);
+            li.appendChild(span);
+            closeElement();
+        }
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
--- laravel-cart/resources/views/home.blade.php
+++ laravel-cart/resources/views/home.blade.php
@@ -1,40 +1,116 @@
 @extends('app')
 @section('content')
-   <h3 class="h3">Product List</h3>
-   <div class="row">
-       @forelse($products as $product)
-           <div class="col-md-3 col-sm-6">
-               <div class="product-grid">
-                   <div class="product-image">
-                       <a href="#">
-                           <img class="pic-1" src="{{asset('images/'.$product->image)}}">
-                       </a>
-                       <ul class="social">
-                           <li><a href="" data-tip="Quick View"><i class="fa fa-search"></i></a></li>
-                           <li><a href="{{ route('add-to-cart', $product->id) }}" data-tip="Add to Cart">
-                               <i class="fa fa-shopping-cart"></i></a></li>
-                       </ul>
-                       <span class="product-new-label">Sale</span>
-                       <span class="product-discount-label">20%</span>
-                   </div>
-                   <ul class="rating">
-                       <li class="fa fa-star"></li>
-                       <li class="fa fa-star"></li>
-                       <li class="fa fa-star"></li>
-                       <li class="fa fa-star"></li>
-                       <li class="fa fa-star disable"></li>
-                   </ul>
-                   <div class="product-content">
-                       <h3 class="title"><p href="#">{{ $product->name }}</p></h3>
-                       <div class="price">{{ number_format($product->price) }}
-                           <!-- <span>$20.00</span> -->
-                       </div>
-                       <a class="add-to-cart" href="{{ route('add-to-cart', $product->id) }}">+ Add To Cart</a>
-                   </div>
-               </div>
-           </div>
-       @empty
-           <p>{{ "Không có sản phẩm nào" }}</p>
-       @endforelse
-   </div>
+<h3 class="h3">Product List</h3>
+<div class="row">
+    @forelse($products as $product)
+    <div class="col-md-3 col-sm-6">
+        <div class="product-grid">
+            <div class="product-image">
+                <a href="#">
+                    <img class="pic-1" src="{{asset('images/'.$product->image)}}">
+                </a>
+                <ul class="social">
+                    <li><a href="" data-tip="Quick View"><i class="fa fa-search"></i></a></li>
+                    <li><a href="{{ route('add-to-cart', $product->id) }}" data-tip="Add to Cart">
+                            <i class="fa fa-shopping-cart"></i></a></li>
+                </ul>
+                <span class="product-new-label">Sale</span>
+                <!-- <span class="product-discount-label">20%</span> -->
+            </div>
+            <ul class="rating">
+                <li class="fa fa-star"></li>
+                <li class="fa fa-star"></li>
+                <li class="fa fa-star"></li>
+                <li class="fa fa-star"></li>
+                <li class="fa fa-star disable"></li>
+            </ul>
+            <div class="product-content">
+                <h3 class="title">
+                    <p href="#">{{ $product->name }}</p>
+                </h3>
+                <div class="price">{{ number_format($product->price) }}
+                    <!-- <span>$20.00</span> -->
+                </div>
+                <a class="add-to-cart" href="{{ route('add-to-cart', $product->id) }}">+ Add To Cart</a>
+            </div>
+        </div>
+
+    </div>
+    @empty
+    <p>{{ "Không có sản phẩm nào" }}</p>
+    @endforelse
+</div>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!--Card-->
+<div class="card shadow mb-5 bg-white rounded">
+    <!--Card-Body-->
+    <div class="card-body">
+        <!--Card-Title-->
+           
+        <h3 class="card-title text-center shadow mb-5 rounded">Đặt vé tàu hỏa  <i class="fa fa-train" aria-hidden="true"></i></h3>
+        <hr>
+        <p class="searchText"><strong>Tìm chuyến</strong></p>
+        <!--First Row-->
+        <div style="padding-left: 15px;" class="row mb-3 mt-3"> <label class="radiobtn"><input type="radio" name="gender" value="male"> Một chiều</label> <label style="padding-left:15px;" class="radiobtn"><input type="radio" name="gender" value="male"> Khứ hồi</label> </div>
+        <!--Second Row-->
+        <div class="row">
+            <div class="col-sm-6"> <select class="browser-default custom-select mb-4" id="select">
+                    <option value="" disabled="" selected="">Ga đi</option>
+                    <option value="1">New Delhi</option>
+                    <option value="2">Mumbai</option>
+                    <option value="3">Bangalore</option>
+                </select> </div>
+            <div class="col-sm-6">
+                <select class="browser-default custom-select mb-4" id="select">
+                    <option value="" disabled="" selected="">Ga đến</option>
+                    <option value="1">New Delhi</option>
+                    <option value="2">Mumbai</option>
+                    <option value="3">Bangalore</option>
+                </select>
+            </div>
+        </div>
+        <!--Third Row-->
+        <div class="row">
+            <div class="col-sm-6"> <input placeholder="&#xf073; Ngày đi" type="text" id="date-picker-example" class="form-control datepicker mb-4" style="font-family:Arial, FontAwesome">
+         </div>
+            <div class="col-sm-6"> <input placeholder="&#xf073; Ngày về" type="text" id="date-picker-example" class="form-control datepicker" style="font-family:Arial, FontAwesome"> </div>
+        </div>
+        <!--Fourth Row-->
+        <!-- <div class="row mt-4">
+            <div class="col-sm-6"> <select class="browser-default custom-select mb-4" id="select">
+                    <option value="" disabled="" selected="">Anytime</option>
+                    <option value="1">6:00 AM</option>
+                    <option value="2">3:00 PM</option>
+                    <option value="3">6:00 PM</option>
+                </select> </div>
+            <div class="col-sm-6"> <select class="browser-default custom-select mb-4" id="select">
+                    <option value="" disabled="" selected="">Anytime</option>
+                    <option value="1">6:00 AM</option>
+                    <option value="2">3:00 PM</option>
+                    <option value="3">6:00 PM</option>
+                </select> </div>
+        </div> -->
+        <!--Fifth Row-->
+        <!-- <div class="row">
+            <div class="col-sm-4"> <select class="browser-default custom-select mb-4" id="select">
+                    <option value="" disabled="" selected="">Kids(0-14)</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select> </div>
+            <div class="col-sm-4"> <select class="browser-default custom-select mb-4" id="select">
+                    <option value="" disabled="" selected="">Adults(15-64)</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select> </div>
+            <div class="col-sm-4"> <select class="browser-default custom-select mb-4" id="select">
+                    <option value="" disabled="" selected="">Seniors(65+)</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select> </div> -->
+        </div> <a href="#" class="btn btn-primary float-right mt-5">Tìm kiếm</a>
+    </div>
+</div>
 @endsection
